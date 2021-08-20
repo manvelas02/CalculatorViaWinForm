c4cc7603cb87d638af9f720e740162703026595f
manvelas02 <manvelasat@gmail.com>
2021-08-20 16:29:36 +0400

    Fixing a few bugs

--- CalculatorViaWinForm/MainForm.Designer.cs
+++ CalculatorViaWinForm/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CalculatorViaWinForm
 {
-    partial class Form1
+    partial class CalculatorWinForm
     {
         /// <summary>
         /// Required designer variable.
@@ -315,7 +315,7 @@ namespace CalculatorViaWinForm
             this.finalLabel.TabIndex = 2;
             this.finalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // Form1
+            // CalculatorWinForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -323,8 +323,9 @@ namespace CalculatorViaWinForm
             this.Controls.Add(this.finalLabel);
             this.Controls.Add(this.enterTextBox);
             this.Controls.Add(this.buttonsPanel);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "CalculatorWinForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
             this.buttonsPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
---
+++ CalculatorViaWinForm/MainForm.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace CalculatorViaWinForm
+{
+    public partial class CalculatorWinForm : Form
+    {
+        public CalculatorWinForm()
+        {
+            InitializeComponent();
+        }
+        int dotCount = 0;
+        double fNum = 0;
+        double sNum = 1;
+        bool isChangedText = false;
+        bool isOp = false;
+        private void IsTextChage(object sender, EventArgs e)
+        {
+            isChangedText = true;
+        }
+        private void DontTypeTextBox(object sender, KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                e.Handled = true;
+        }
+        public bool IsOperation(string source)
+        {
+            bool res = false;
+            int opIndex;
+            if (source.Length == 0)
+            {
+                return res;
+            }
+            else
+            {
+                opIndex = source.Length - 2;
+            }
+            if (source[opIndex].ToString() == plusBtn.Text ||
+                source[opIndex].ToString() == minusBtn.Text ||
+                source[opIndex].ToString() == devideBtn.Text ||
+                source[opIndex].ToString() == multiplyBtn.Text ||
+                source[opIndex].ToString() == equalBtn.Text)
+            {
+                res = true;
+            }
+            return res;
+        }
+        private void InputNumber(object sender, EventArgs e)
+        {
+            if (this.enterTextBox.Text.Length < 13)
+            {
+                string senderText = ((Button)sender).Text;
+                if (senderText == dotBtn.Text && this.enterTextBox.Text != "Error")
+                {
+                    if (dotCount < 1)
+                    {
+                        this.enterTextBox.Text += senderText;
+                        dotCount++;
+                    }
+                }
+                else
+                {
+                    if (!String.IsNullOrWhiteSpace(this.finalLabel.Text))
+                    {
+                        if (this.finalLabel.Text[this.finalLabel.Text.Length - 2].ToString()
+                        == equalBtn.Text)
+                        {
+                            Clear((object)clearAllBtn, e);
+                            if (senderText == dotBtn.Text)
+                            {
+                                if (dotCount < 1)
+                                {
+                                    this.enterTextBox.Text += senderText;
+                                    dotCount++;
+                                }
+                            }
+                            else
+                            {
+                                this.enterTextBox.Text = senderText;
+                                dotCount = 0;
+                            }
+                            isOp = false;
+                        }
+                        else if (IsOperation(finalLabel.Text) && isOp)
+                        {
+                            this.enterTextBox.Text = "";
+                            this.enterTextBox.Text += senderText;
+                            dotCount = 0;
+                            isOp = false;
+                        }
+                        else if (this.enterTextBox.Text == "0" || this.enterTextBox.Text == "Error")
+                        {
+                            this.enterTextBox.Text = senderText;
+                        }
+                        else
+                        {
+                            this.enterTextBox.Text += senderText;
+                        }
+                    }
+                    else if (this.enterTextBox.Text == "0" || this.enterTextBox.Text == "Error")
+                    {
+                        this.enterTextBox.Text = senderText;
+                    }
+                    else
+                    {
+                        this.enterTextBox.Text += senderText;
+                    }
+                }
+            }
+        }
+        public string ChangeText(string senderText, string source)
+        {
+            string tempStr = null;
+            for (int i = 0; i < source.Length - 2; i++)
+            {
+                tempStr += source[i].ToString();
+            }
+            tempStr += senderText + " ";
+            return tempStr;
+        }
+        Func<double, double, string> lastOp;
+        private void Operation(string operation, Func<double, double, string> op)
+        {
+            if (lastOp == null)
+            {
+                lastOp = op;
+            }
+            if (this.finalLabel.Text.Length < 32)
+            {
+                if (String.IsNullOrWhiteSpace(this.finalLabel.Text))
+                {
+                    if (this.enterTextBox.Text[this.enterTextBox.Text.Length - 1].ToString()
+                        == dotBtn.Text)
+                    {
+                        string tempStr = null;
+                        for (int i = 0; i < this.enterTextBox.Text.Length - 1; i++)
+                        {
+                            tempStr += this.enterTextBox.Text[i].ToString();
+                        }
+                        this.enterTextBox.Text = tempStr;
+                        fNum = double.Parse(tempStr);
+                        this.finalLabel.Text += tempStr + " " + operation + " ";
+                        isChangedText = false;
+                        isOp = true;
+                    }
+                    else
+                    {
+                        fNum = double.Parse(this.enterTextBox.Text);
+                        this.finalLabel.Text += this.enterTextBox.Text + " " + operation + " ";
+                        isChangedText = false;
+                        isOp = true;
+                    }
+                }
+                if (isChangedText)
+                {
+                    if (this.enterTextBox.Text[this.enterTextBox.Text.Length - 1].ToString()
+                        == dotBtn.Text)
+                    {
+                        string tempStr = null;
+                        for (int i = 0; i < this.enterTextBox.Text.Length - 1; i++)
+                        {
+                            tempStr += this.enterTextBox.Text[i].ToString();
+                        }
+                        sNum = double.Parse(tempStr);
+                        this.finalLabel.Text += tempStr + " " + operation + " ";
+                        this.enterTextBox.Text = lastOp(fNum, sNum);
+                        fNum = double.Parse(this.enterTextBox.Text);
+                        lastOp = op;
+                        isOp = true;
+                        isChangedText = false;
+                    }
+                    else if (operation == equalBtn.Text)
+                    {
+                        sNum = double.Parse(this.enterTextBox.Text);
+                        this.finalLabel.Text += this.enterTextBox.Text + " " + operation + " ";
+                        this.enterTextBox.Text = lastOp(fNum, sNum);
+                        if (this.enterTextBox.Text == "Error")
+                        {
+                            fNum = 0;
+                        }
+                        else
+                        {
+                            fNum = double.Parse(this.enterTextBox.Text);
+                        }
+                        lastOp = op;
+                        isOp = true;
+                        isChangedText = false;
+                    }
+                    else
+                    {
+                        sNum = double.Parse(this.enterTextBox.Text);
+                        this.finalLabel.Text += this.enterTextBox.Text + " " + operation + " ";
+                        this.enterTextBox.Text = lastOp(fNum, sNum);
+                        fNum = double.Parse(lastOp(fNum, sNum));
+                        lastOp = op;
+                        isOp = true;
+                        isChangedText = false;
+                    }
+                }
+                else
+                {
+                    this.finalLabel.Text = ChangeText(operation, this.finalLabel.Text);
+                    lastOp = op;
+                    isChangedText = false;
+                }
+            }
+            else
+            {
+                this.finalLabel.Text = lastOp(fNum, sNum) + " " + operation + " ";
+                this.enterTextBox.Text = lastOp(fNum, sNum);
+                fNum = double.Parse(lastOp(fNum, sNum));
+                lastOp = op;
+                isOp = true;
+                isChangedText = false;
+            }
+        }
+        private void Operations(object sender, EventArgs e)
+        {
+            string senderText = ((Button)sender).Text;
+            Calculator calculator = new Calculator();
+            if (senderText == plusBtn.Text)
+            {
+                Operation(senderText, calculator.Plus);
+            }
+            else if (senderText == minusBtn.Text)
+            {
+                Operation(senderText, calculator.Minus);
+            }
+            else if (senderText == multiplyBtn.Text)
+            {
+                Operation(senderText, calculator.Multiply);
+            }
+            else if (senderText == devideBtn.Text)
+            {
+                Operation(senderText, calculator.Devide);
+            }
+            else if (senderText == equalBtn.Text)
+            {
+                Operation(senderText, lastOp);
+            }
+        }
+        public void Clear(object sender, EventArgs e)
+        {
+            string senderText = ((Button)sender).Text;
+            if (!String.IsNullOrWhiteSpace(this.finalLabel.Text))
+            {
+                if (senderText == clearAllBtn.Text ||
+                   (senderText == clearEntryBtn.Text &&
+                   this.finalLabel.Text[this.finalLabel.Text.Length - 2].ToString()
+                   == equalBtn.Text))
+                {
+                    fNum = 0;
+                    sNum = 1;
+                    this.finalLabel.Text = "";
+                    this.enterTextBox.Text = "0";
+                    if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                    {
+                        dotCount = 0;
+                    }
+                }
+                else if (senderText == clearEntryBtn.Text)
+                {
+                    sNum = 1;
+                    this.enterTextBox.Text = "0";
+                    if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                    {
+                        dotCount = 0;
+                    }
+                }
+                else if (senderText == removeBtn.Text)
+                {
+                    if (isChangedText)
+                    {
+                        string tempStr = null;
+                        for (int i = 0; i < this.enterTextBox.Text.Length - 1; i++)
+                        {
+                            tempStr += this.enterTextBox.Text[i].ToString();
+                        }
+                        if (tempStr == null)
+                        {
+                            this.enterTextBox.Text = "0";
+                        }
+                        else
+                        {
+                            this.enterTextBox.Text = tempStr;
+                        }
+                        if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                        {
+                            dotCount = 0;
+                        }
+                    }
+                    else
+                    {
+                        if (!String.IsNullOrWhiteSpace(this.finalLabel.Text))
+                        {
+                            if (this.finalLabel.Text[this.finalLabel.Text.Length - 2].ToString() == equalBtn.Text)
+                            {
+                                this.finalLabel.Text = "";
+                                if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                                {
+                                    dotCount = 0;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            string tempStr = null;
+                            for (int i = 0; i < this.enterTextBox.Text.Length - 1; i++)
+                            {
+                                tempStr += this.enterTextBox.Text[i].ToString();
+                            }
+                            if (tempStr == null)
+                            {
+                                this.enterTextBox.Text = "0";
+                            }
+                            else
+                            {
+                                this.enterTextBox.Text = tempStr;
+                            }
+                            if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                            {
+                                dotCount = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (senderText == clearEntryBtn.Text)
+            {
+                sNum = 1;
+                this.enterTextBox.Text = "0";
+                if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                {
+                    dotCount = 0;
+                }
+            }
+            else if (senderText == removeBtn.Text)
+            {
+                if (isChangedText)
+                {
+                    string tempStr = null;
+                    for (int i = 0; i < this.enterTextBox.Text.Length - 1; i++)
+                    {
+                        tempStr += this.enterTextBox.Text[i].ToString();
+                    }
+                    if (tempStr == null)
+                    {
+                        this.enterTextBox.Text = "0";
+                    }
+                    else
+                    {
+                        this.enterTextBox.Text = tempStr;
+                    }
+                    if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                    {
+                        dotCount = 0;
+                    }
+                }  
+                else
+                {
+                    if (!String.IsNullOrWhiteSpace(this.finalLabel.Text))
+                    {
+                        if (this.finalLabel.Text[this.finalLabel.Text.Length - 2].ToString() == equalBtn.Text)
+                        {
+                            this.finalLabel.Text = "";
+                            if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                            {
+                                dotCount = 0;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        string tempStr = null;
+                        for (int i = 0; i < this.enterTextBox.Text.Length - 1; i++)
+                        {
+                            tempStr += this.enterTextBox.Text[i].ToString();
+                        }
+                        if (tempStr == null)
+                        {
+                            this.enterTextBox.Text = "0";
+                        }
+                        else
+                        {
+                            this.enterTextBox.Text = tempStr;
+                        }
+                        if (!this.enterTextBox.Text.Contains(dotBtn.Text))
+                        {
+                            dotCount = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    public class Calculator
+    {
+        public string Plus(double fNum, double sNum)
+        {
+            return (fNum + sNum).ToString();
+        }
+        public string Minus(double fNum, double sNum)
+        {
+            return (fNum - sNum).ToString();
+        }
+        public string Devide(double fNum, double sNum)
+        {
+            if (sNum == 0)
+            {
+                return "Error";
+            }
+            return (fNum / sNum).ToString();
+        }
+        public string Multiply(double fNum, double sNum)
+        {
+            return (fNum * sNum).ToString();
+        }
+    }
+}
